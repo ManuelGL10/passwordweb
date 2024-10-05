3878c1d57a78fb13bd2449d9ce1ba68c4d1e51ea
Luis Manuel Gomez <132625020+ManuelGL10@users.noreply.github.com>
2024-10-04 21:41:13 -0600

    Elimanacion dependencias

--- src/views/DashBoard.jsx
+++ src/views/DashBoard.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import DBNavBar from "../components/DBNavBar";
 import PasswordTable from "../components/PasswordTable";
 
 const DashBoard = () => {
     return(
         <div>
-            <DBNavBar/>
             <PasswordTable/>
         </div>
     );
