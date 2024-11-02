0155b938179c7b85b6dc0edd8d4b6c56552bb0b2
Manuel Gómez <luismanuelgomezlopez18@gmail.com>
2024-11-01 19:13:53 -0600

    Actualización de Notificaciones

--- public/sw.js
+++ public/sw.js
@@ -1,92 +1,4 @@
 
-self.addEventListener('sync', (event) => {
-    if(event.tag === 'sync-passwords'){
-        event.waitUntil(
-            syncPasswords() //Sincronizar las tareas cuando haya conexión 
-        );
-    }
-});
-
-function syncPasswords(){
-    return new Promise((resolve, reject) =>  {
-        const dbRequest = indexedDB.open('passwordDB'); 
-
-        dbRequest.onsuccess = event => {
-            const db = event.target.result; 
-            const transaction = db.transaction('passwords', 'readonly');
-            const objectStore = transaction.objectStore('passwords');
-            const getAllRequest = objectStore.getAll();
-
-            getAllRequest.onsuccess = () => {
-                const passwords = getAllRequest.result;
-
-                if(passwords.length === 0){
-                    console.log('No hay tareas para sincronizar.');
-                    return resolve(); 
-                }
-
-                // Enviar cada tarea al backend 
-                const promises = passwords.map(password => {
-                    return fetch('http://localhost:4000/post_passwords', {
-                        method: 'POST', 
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }, 
-                        body: JSON.stringify(password)
-                    })
-                    .then(response => {
-                        if(!response.ok){
-                            throw new Error('Error en la API'); 
-                        }
-                        return response.json(); 
-                    })
-                    .then(data => {
-                        console.log('Tarea sincronizada con éxito:', data); 
-                        eliminarPasswordById(password.id);//Elimina tarea de Indexed tas sincronización
-                    })
-                    .catch(error => {
-                        console.error('Error al sicronizar tarea:', error); 
-                    }); 
-                });
-
-                Promise.all(promises).then(() => resolve()).catch(reject);
-            }; 
-
-            getAllRequest.onerror = event => {
-                console.error('Error al obtener tareas de IndexedDB', event);
-                reject(); 
-            };
-        }; 
-
-        dbRequest.onerror = event => {
-            console.error('Error al abrir la base de datos:', event);
-            reject(); 
-        };
-    }); 
-}
-
-// Funcion para elimanar una tarea de IndexedDB
-
-function eliminarPasswordById(id){
-    let dbRequest = indexedDB.open('passwordDB'); 
-
-    dbRequest.onsuccess = event => {
-        let db = event.target.result;
-        let transaction = db.transaction('passwords', 'readonly'); 
-        let objectStore = transaction.objectStore('passwords'); 
-
-        let deleteRequest = objectStore.delete(id); 
-
-        deleteRequest.onsuccess = () => {
-            console.log(`Tarea con ID ${id} eliminada de IndexedDB`); 
-        };
-
-        deleteRequest.onerror = () =>{
-            console.error('Error al eliminar tarea de IndexedDB', event);
-        };
-    };
-}
-
 const APP_SHELL_CACHE = 'appShell'; //Nombre de la caché principal
 const DYNAMIC_CACHE = 'dynamic'; // Nombre de la caché dinamica
 // Array de las rutas de los archivos esenciales
@@ -117,17 +29,23 @@ self.addEventListener('install', (event) => {
 
 // Activación del SW y limpieza de cachés
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        // Obtiene los nombres de las cachés almacenadas
-        caches.keys().then((keys) =>
-            Promise.all(
-            keys
-                .filter((key) => key !== APP_SHELL_CACHE && key !== DYNAMIC_CACHE)
-                .map((key) => caches.delete(key)) // Borra los cachpes que no sean los actuales
-            )
-        )
-    );
-});
+    //Obtiene los nombres de las caches almacenadas 
+    caches.keys().then((keys) => 
+        keys
+            .filter((keys) => key !== APP_SHELL_CACHE && key !== DYNAMIC_CACHE)
+            .map((key) => caches.delete(key)) // Borra los caches que no sean actuales 
+    )
+}); 
+
+//Listener para push
+self.addEventListener('push', event => {
+    const opciones = {
+        body:"mensaje", 
+        icon:'/icon/icon1024.png',
+        silent:null
+    }
+    self.registration.showNotification('Confirmado, Luis es gay', opciones)
+})
 
 self.addEventListener('fetch', event => {
     const resp = fetch(event.request).then(respuesta => {
@@ -167,8 +85,8 @@ self.addEventListener('fetch', event => {
 self.addEventListener('sync', event => {
     console.log('Sync event triggered', event);
 
-    if (event.tag === 'sync-users'){
-        event.waitUntil(syncPasswords());
+    if (event.tag === 'sync-passwords'){
+        syncPasswords();
     }
 }); 
 
@@ -188,7 +106,6 @@ function syncPasswords() {
 
                 if(passwords.length === 0){
                     console.log('No hay tareas para sincronizar.'); 
-                    return resolve();
                 }
 
                 // Enviar cada tarea al backend
@@ -208,7 +125,7 @@ function syncPasswords() {
                     })
                     .then(data => {
                         console.log('Tarea sincronizada con éxito:', data);
-                        eliminarPassword();
+                        eliminarPassword(password.id);
                     })
                     .catch(error => {
                         console.error('Error al sincronizar tarea:', error);
@@ -232,42 +149,23 @@ function syncPasswords() {
     });
 }
 
+
 // Función para elimanar usuarios de IndexedDB
-function eliminarPassword() {
-    let db = indexedDB.open('passwordDB')
+function eliminarPassword(id){
+    const request = indexedDB.open('passwordDB'); 
 
-    db.onsuccess = event => {
-        let result = event.target.result; 
+    request.onerror = (event) => {
+        console.error('Error abriendo la base de datos', event); 
+    }; 
 
-        let transaction = result.transaction('passwords', 'readwrite');
-        let obj = transaction.objectStore('passwords'); 
+    request.onsuccess = (event) => {
+        const db = event.target.result; 
+        const transaction = db.transaction('passwords', 'readwrite');
+        const objectStore = transaction.objectStore('passwords'); 
+        const deleteRequest = objectStore.delete(id); 
 
-        let cursorRequest = obj.openCursor();
-
-        cursorRequest.onsuccess = event => {
-            let cursor = event.target.result;
-
-            if (cursor) {
-                // Mostramos el registro en la consola
-                console.log('Contraseña encontrado:', cursor.value);
-    
-                // Eliminamos el registro actual
-                let deleteRequest = cursor.delete();
-    
-                deleteRequest.onsuccess = () => {
-                    console.log(`Contraseña con ID ${cursor.value.id} eliminado`);
-                };
-    
-                // Continuamos con el siguiente registro
-                cursor.continue();
-            } else {
-                // No quedan más registros
-                console.log('No hay más Contraseñas por eliminar.');
-            }
-        }
-
-        cursorRequest.onerror = event => {
-            console.error('Error al abrir el cursor:', event);
+        deleteRequest.onsuccess = () => {
+            console.log(`Registro con id ${id} eliminado`); 
         };
-    }
+    };
 }
